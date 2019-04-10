6ddad68bac0fd66182655e33c67004c5b6252e47
Farhad Keramati <farhadkm94@yahoo.com>
2019-04-10 17:07:30 -0400

    console for adding sensor and actuators and configuring them

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "react-tabs/style/react-tabs.css";
+import Dashboard from './Dashboard';
+import Settings from './Settings';
 
 class App extends Component {
+
+  state = {
+    showUrl: false,
+    url: '',
+    send_data: false,
+    airQaulity: 0,
+    motion: false,
+    sound: 0,
+    moister: 0
+  }
+
+  componentDidMount = () => {
+    
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+      <Tabs>
+        <TabList>
+          <Tab>Dashboard</Tab>
+          <Tab>Setup</Tab>
+        </TabList>
+
+        <TabPanel>
+          <Dashboard />
+        </TabPanel>
+        <TabPanel>
+          <Settings />
+        </TabPanel>
+      </Tabs>
+
     );
   }
 }
+
 
 export default App;
